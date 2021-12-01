2259c5b428ed25efcb6fadb3c348589161b6a64e
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2021-12-01 15:12:41 +0100

    Update vw_constructions_genie_civil

--- views/vw_constructions_genie_civil_projet.sql
+++ views/vw_constructions_genie_civil_projet.sql
@@ -3,16 +3,28 @@ DROP VIEW IF EXISTS export.vw_constructions_genie_civil_projet;
 CREATE OR REPLACE VIEW export.vw_constructions_genie_civil_projet AS
 
 SELECT
-    enfv.id_enfv as id_enfv,
-	enfv.libelle_enfv as libelle,
-	enfv.classcode_enfv as classe,
-	prj.id_prj AS projet_id,
-	prj.nom_prj AS projet_nom,
-	prj.description_prj AS projet_description,
-	prj.etat_prj AS projet_etat,
-	ST_Force2D(enfv.the_geom)::geometry('Polygon',2056) as geom_polygon,
-	st_centroid(ST_Force2D(enfv.the_geom))::geometry('Point',2056) as geom_centroid
+    obrv.id_obrv as id_obrv,
+	obrv.idobr_obrv as id_obr,
+	obrv.modele_obrv AS modele,
+	orc.libelle_orc AS classe,
+	obrv.nom_obrv as nom,
+	eta.libelle_eta as etat,
+	obrv.constructiondate_obrv as date_construction,
+	obrv.miseenservicedate_obrv as date_mise_en_service,
+	--ndfv.niveautension_ndf as tension,
+	prap.libelle_pra as proprietaire,
+	prae.libelle_pra as exploitant,
+	nodv.geoposz_nodv as altitude,
+	obrv.fictif_obrv as fictif,
+	--st_centroid(ST_Force2D(ndfv.the_geom))::geometry('Point',2056) as geom_centroid,
+	ST_Force2D(ndfv.the_geom)::geometry('Polygon',2056) as geom_polygon
 	
-FROM dbo.externalnoeudfeatureversion_enfv enfv
-LEFT JOIN dbo.projet_prj prj ON prj.id_prj = enfv.idprj_enfv
-WHERE id_prj != 1;
+FROM dbo.objetreseauversion_obrv obrv
+	LEFT JOIN dbo.noeudversion_nodv nodv ON nodv.id_obrv = obrv.id_obrv
+	LEFT JOIN dbo.nobjetreseauclasse_orc orc ON orc.id_orc = obrv.idorc_obrv
+	LEFT JOIN dbo.noeudfeatureversion_ndfv ndfv ON  ndfv.idobr_ndfv = obrv.idobr_obrv
+	LEFT JOIN dbo.netat_eta eta ON  eta.id_eta = obrv.idetat_obrv
+	LEFT JOIN dbo.npersonneabstraite_pra prap ON obrv.idproprietairepra_obrv = prap.id_pra
+	LEFT JOIN dbo.npersonneabstraite_pra prae ON obrv.idexploitantpra_obrv = prae.id_pra
+	LEFT JOIN dbo.projet_prj prj ON prj.id_prj = obrv.idprj_obrv
+WHERE obrv.idorc_obrv = 46 and obrv.idprj_obrv != 1 and ndfv.idprj_ndfv != 1; -- 46 chambre
