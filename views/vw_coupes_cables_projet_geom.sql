840288c5df13e3294ebb62db37e3f8620c869d18
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2022-01-20 15:33:15 +0100

    Force 2d for views prerquisites

--- views/vw_coupes_cables_projet_geom.sql
+++ views/vw_coupes_cables_projet_geom.sql
@@ -4,7 +4,7 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS export.vw_coupes_cables_projet_geom
 TABLESPACE pg_default
 AS
  SELECT cupv.idobr_cupv AS id_obr,
-    st_union(cupv.the_geom::geometry(Polygon,2056)) AS geom_multi_polygon
+    st_union(ST_Force2D(cupv.the_geom)::geometry(Polygon,2056)) AS geom_multi_polygon
    FROM dbo.coupefeatureversion_cupv cupv
   WHERE cupv.coupetype_cupv = 4 AND cupv.idprj_cupv != 1
   GROUP BY cupv.idobr_cupv
