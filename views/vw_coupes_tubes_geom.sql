d40b3fd2529e715cc89ab787bb22e261181a3eb6
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2022-03-07 13:49:49 +0100

    Fix geomtype for coupes
    Fix geomtype for coupes

--- views/vw_coupes_tubes_geom.sql
+++ views/vw_coupes_tubes_geom.sql
@@ -4,7 +4,7 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS export.vw_coupes_tubes_geom
 TABLESPACE pg_default
 AS
  SELECT cupv.idobr_cupv AS id_obr,
-    st_union(ST_Force2D(cupv.the_geom)::geometry(Polygon,2056)) AS geom_multi_polygon
+    ST_MULTI(ST_UNION(ST_Force2D(cupv.the_geom)::geometry(Polygon,2056))) AS geom_multi_polygon
    FROM dbo.coupefeatureversion_cupv cupv
   WHERE cupv.coupetype_cupv = 2 AND cupv.idprj_cupv = 1
   GROUP BY cupv.idobr_cupv
--- views/vw_zones_projet.sql
+++ views/vw_zones_projet.sql
@@ -1,3 +1,4 @@
+
 DROP VIEW IF EXISTS export.vw_zones_projets;
 
 CREATE OR REPLACE VIEW export.vw_zones_projets AS
@@ -9,7 +10,11 @@ SELECT
 	prj.nom_prj AS projet_nom,
 	prj.description_prj AS projet_description,
 	prj.etat_prj AS projet_etat,
-	ST_Force2D(zft.the_geom) as geom_polygon
+	CASE 
+		WHEN ST_geometrytype(the_geom) = 'ST_Point' 
+			THEN ST_Buffer(ST_SetSRID(the_geom,2056),10)
+		ELSE ST_ForceCurve(ST_SetSRID(the_geom,2056))
+	END geometry_polygon
 	
 FROM dbo.zonefeature_zft zft
 	LEFT JOIN dbo.projet_prj prj ON prj.nom_prj = zft.label_zft
