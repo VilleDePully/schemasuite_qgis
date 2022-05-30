c104ab75cf531e20c686a7d6e8bc0e6a844b9178
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2022-05-30 16:06:19 +0200

    Ajout des attributs pour armoires, candélabres et stations
    Ajout des attributs pour armoires, candélabres et stations

--- postgresql/views/vw_candelabres.sql
+++ postgresql/views/vw_candelabres.sql
@@ -3,19 +3,27 @@ DROP VIEW IF EXISTS export.vw_candelabres;
 CREATE OR REPLACE VIEW export.vw_candelabres AS
 
 SELECT
-    obrv.id_obrv as id_obrv,
+	obrv.id_obrv as id_obrv,
 	obrv.idobr_obrv as id_obr,
 	obrv.modele_obrv AS modele,
+	obrv.code_obrv AS code,
 	obrv.nom_obrv as nom,
-	eta.libelle_eta as etat,
+	obrv.racineguid_obrv as guid_racine,
+	eta.libelle_eta as etat_deploiement,
+	ete.libelle_ete as etat_entretien,
+	obrv.convention_obrv as num_esti,
 	obrv.constructiondate_obrv as date_construction,
 	obrv.miseenservicedate_obrv as date_mise_en_service,
-	obrv.observation_obrv as remarque,
+	obrv.observation_obrv as observation,
 	ndfv.niveautension_ndf as tension,
 	prap.libelle_pra as proprietaire,
 	prae.libelle_pra as exploitant,
+	praf.libelle_pra as fournisseur,
 	nodv.geoposz_nodv as altitude,
 	nodv.acces_nodv as acces,
+	nodv.emplacement_nodv as emplacement,
+	nodv.remarquecontrole_nodv as remarque_controle,
+	nodv.datedecontrole_nodv as date_controle,
 	obrv.creationdate_obrv AS date_creation,
 	obrv.modificationdate_obrv AS date_modification,
 	st_centroid(ST_Force2D(ndfv.the_geom))::geometry('Point',2056) as geom_centroid,
@@ -25,8 +33,10 @@ FROM dbo.objetreseauversion_obrv obrv
 	LEFT JOIN dbo.noeudversion_nodv nodv ON nodv.id_obrv = obrv.id_obrv
 	LEFT JOIN dbo.noeudfeatureversion_ndfv ndfv ON  ndfv.idobr_ndfv = obrv.idobr_obrv
 	LEFT JOIN dbo.netat_eta eta ON  eta.id_eta = obrv.idetat_obrv
+	LEFT JOIN dbo.netatentretien_ete ete ON  ete.id_ete = obrv.idetatentretien_obrv
 	LEFT JOIN dbo.npersonneabstraite_pra prap ON obrv.idproprietairepra_obrv = prap.id_pra
 	LEFT JOIN dbo.npersonneabstraite_pra prae ON obrv.idexploitantpra_obrv = prae.id_pra
+	LEFT JOIN dbo.npersonneabstraite_pra praf ON obrv.idfournisseurpra_obrv = praf.id_pra
 	LEFT JOIN dbo.projet_prj prj ON prj.id_prj = obrv.idprj_obrv
 WHERE obrv.idorc_obrv = 17 
 	AND obrv.idprj_obrv = 1 
