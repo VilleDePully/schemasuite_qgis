79ff83c3afe1045678783be119e99fe93d671a13
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2022-05-31 16:16:57 +0200

    Ajout des attributs complémentaires pour les coffrets
    Ajout des attributs complémentaires pour les coffrets

--- postgresql/views/vw_coffrets_projet.sql
+++ postgresql/views/vw_coffrets_projet.sql
@@ -3,26 +3,47 @@ DROP VIEW IF EXISTS export.vw_coffrets_projet;
 CREATE OR REPLACE VIEW export.vw_coffrets_projet AS
 
 SELECT
-    obrv.id_obrv as id_obrv,
+	obrv.id_obrv as id_obrv,
 	obrv.idobr_obrv as id_obr,
 	obrv.modele_obrv AS modele,
+	obrv.code_obrv AS code,
 	obrv.nom_obrv as nom,
-	eta.libelle_eta as etat,
-	cof.intensiteinstalle_cof as intensite_installee,
+	obrv.racineguid_obrv as guid_racine,
+	eta.libelle_eta as etat_deploiement,
+	ete.libelle_ete as etat_entretien,
+	prt.libelle_prt	as type_propriete,
+	obrv.convention_obrv as num_esti,
 	obrv.constructiondate_obrv as date_construction,
 	obrv.miseenservicedate_obrv as date_mise_en_service,
-	obrv.observation_obrv as remarque,
+	obrv.observation_obrv as observation,
 	ndfv.niveautension_ndf as tension,
 	prap.libelle_pra as proprietaire,
 	prae.libelle_pra as exploitant,
+	praf.libelle_pra as fournisseur,
 	nodv.geoposz_nodv as altitude,
 	nodv.acces_nodv as acces,
+	nodv.emplacement_nodv as emplacement,
+	nodv.remarquecontrole_nodv as remarque_controle,
+	nodv.datedecontrole_nodv as date_controle,
 	prj.id_prj AS projet_id,
 	prj.nom_prj AS projet_nom,
 	prj.description_prj AS projet_description,
 	prj.etat_prj AS projet_etat,
 	obrv.creationdate_obrv AS date_creation,
 	obrv.modificationdate_obrv AS date_modification,
+	cof.impendance_cof AS impedance,
+	cof.icc_cof AS icc,
+	gco.nom_gco AS genre,
+	cof.avecderivation_cof as avec_derivation,
+	cof.typecsgeneral_cof as type_cs_general,
+	cof.nombrefusible_cof as nombre_fusibles,
+	cof.intensitetaxe_cof as intensite_taxe,
+	cof.intensiteinstalle_cof as intensite_installee,
+	cof.intensitemax_cof as intensite_max,
+	cof.intensiteproduction_cof as intensite_production,
+	cof.nopolice_cof as no_police,
+	cof.pmaxfeg_cof as puissance_max_feg,
+	cof.resisolation_cof as resistance_isolation,
 	CASE
 		WHEN obrv.state_obrv = 0 THEN 'Cree'
 		WHEN obrv.state_obrv = 1 THEN 'Modifie'
@@ -33,13 +54,17 @@ SELECT
 	
 FROM dbo.v_objetreseauversionliaison v_obrvl
 	LEFT JOIN dbo.objetreseauversion_obrv obrv ON v_obrvl.id_obrv = obrv.id_obrv
-	LEFT JOIN dbo.coffretintroduction_cof cof ON v_obrvl.id_obrv = cof.id_obrv
 	LEFT JOIN dbo.noeudfeatureversion_ndfv ndfv ON  ndfv.idobr_ndfv = v_obrvl.idparent_cmp
 	LEFT JOIN dbo.noeudversion_nodv nodv ON nodv.id_obrv = v_obrvl.idparent_cmp
 	LEFT JOIN dbo.netat_eta eta ON  eta.id_eta = obrv.idetat_obrv
+	LEFT JOIN dbo.netatentretien_ete ete ON  ete.id_ete = obrv.idetatentretien_obrv
+	LEFT JOIN dbo.nproprietetype_ete prt ON  prt.id_prt = obrv.idproprietetype_obrv
 	LEFT JOIN dbo.npersonneabstraite_pra prap ON obrv.idproprietairepra_obrv = prap.id_pra
 	LEFT JOIN dbo.npersonneabstraite_pra prae ON obrv.idexploitantpra_obrv = prae.id_pra
+	LEFT JOIN dbo.npersonneabstraite_pra praf ON obrv.idfournisseurpra_obrv = praf.id_pra
 	LEFT JOIN dbo.projet_prj prj ON prj.id_prj = obrv.idprj_obrv
+	LEFT JOIN dbo.coffretintroduction_cof cof ON v_obrvl.id_obrv = cof.id_obrv
+	LEFT JOIN dbo.ngenrecoffret_gco gco ON cof.idgco_cof = gco.id_gco
 
 WHERE obrv.idorc_obrv IN (14) 
 	AND obrv.idprj_obrv != 1 
