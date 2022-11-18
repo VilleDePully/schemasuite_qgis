09f2572a9e02d7130191bae17cd1958acbd28b47
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2022-11-18 13:18:56 +0100

    Add script to refresh materialized views
    Add script to refresh materialized views

---
+++ postgresql/scripts/refresh_materialized_views.sql
@@ -0,0 +1,12 @@
+REFRESH MATERIALIZED VIEW export.vw_coupes_cables WITH DATA;
+REFRESH MATERIALIZED VIEW export.vw_coupes_cables_geom WITH DATA;
+REFRESH MATERIALIZED VIEW export.vw_coupes_cables_projet WITH DATA;
+REFRESH MATERIALIZED VIEW export.vw_coupes_cables_projet_geom WITH DATA;
+REFRESH MATERIALIZED VIEW export.vw_coupes_traces WITH DATA;
+REFRESH MATERIALIZED VIEW export.vw_coupes_traces_geom WITH DATA;
+REFRESH MATERIALIZED VIEW export.vw_coupes_traces_projet WITH DATA;
+REFRESH MATERIALIZED VIEW export.vw_coupes_traces_projet_geom WITH DATA;
+REFRESH MATERIALIZED VIEW export.vw_coupes_tubes WITH DATA;
+REFRESH MATERIALIZED VIEW export.vw_coupes_tubes_geom WITH DATA;
+REFRESH MATERIALIZED VIEW export.vw_coupes_tubes_projet WITH DATA;
+REFRESH MATERIALIZED VIEW export.vw_coupes_tubes_projet_geom WITH DATA;
