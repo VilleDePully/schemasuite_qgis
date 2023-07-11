82910156bb1c8fc754a5fa5f4fa9f98cd4aed98c
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2023-07-11 09:19:38 +0200

    Cast Multipolygons for vw_coupes_* elements
    Cast Mulltipolygons for vw_coupes_* elements

--- postgresql/views/vw_coupes_cables_projet_geom.sql
+++ postgresql/views/vw_coupes_cables_projet_geom.sql
@@ -3,7 +3,7 @@ DROP VIEW IF EXISTS export.vw_coupes_cables_projet_geom;
 CREATE OR REPLACE VIEW export.vw_coupes_cables_projet_geom AS
 
  SELECT cupv.idobr_cupv AS id_obr,
-    ST_MULTI(ST_UNION(ST_Force2D(cupv.the_geom)::geometry(Polygon,2056))) AS geom_multi_polygon
+    ST_MULTI(ST_UNION(ST_Force2D(cupv.the_geom)::geometry(Polygon,2056)))::geometry(MultiPolygon,2056) AS geom_multi_polygon
    FROM dbo.coupefeatureversion_cupv cupv
   WHERE cupv.coupetype_cupv = 4 AND cupv.idprj_cupv != 1
   GROUP BY cupv.idobr_cupv;
