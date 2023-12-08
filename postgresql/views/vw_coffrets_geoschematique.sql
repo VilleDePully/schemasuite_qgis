0eefd7fdf623fce1b93362b9bb10b5e69f3c9da6
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2023-12-08 15:52:03 +0100

    Ajouts des composants géoschématiques
    Ajouts des composants géoschématiques

---
+++ postgresql/views/vw_coffrets_geoschematique.sql
@@ -0,0 +1,79 @@
+DROP VIEW IF EXISTS export.vw_coffrets_geoschematique;
+
+CREATE OR REPLACE VIEW export.vw_coffrets_geoschematique AS
+
+SELECT
+	obrv.id_obrv as id_obrv,
+	obrv.idobr_obrv as id_obr,
+	obrv.modele_obrv AS modele,
+	obrv.code_obrv AS code,
+	obrv.nom_obrv as nom,
+	obrv.infobulle_obrv as infobulle,
+	lower(obrv.racineguid_obrv) as guid_racine,
+	eta.libelle_eta as etat_deploiement,
+	ete.libelle_ete as etat_entretien,
+	prt.libelle_prt	as type_propriete,
+	obrv.convention_obrv as num_esti,
+	obrv.constructiondate_obrv as date_construction,
+	obrv.miseenservicedate_obrv as date_mise_en_service,
+	obrv.horsservicedate_obrv as date_mise_hors_service,
+	obrv.observation_obrv as observation,
+	npfv.niveautension_npfv as tension,
+	prap.libelle_pra as proprietaire,
+	prae.libelle_pra as exploitant,
+	praf.libelle_pra as fournisseur,
+	nodv.geoposz_nodv as altitude,
+	nodv.acces_nodv as acces,
+	nodv.emplacement_nodv as emplacement,
+	nodv.typeconstruction_nodv as type_construction,
+	nodv.remarquecontrole_nodv as remarque_controle,
+	nodv.datedecontrole_nodv as date_controle,
+	obrv.creationdate_obrv AS date_creation,
+	obrv.modificationdate_obrv AS date_modification,
+	--Attributs spécifiques
+	cof.impendance_cof AS impedance,
+	cof.icc_cof AS icc,
+	gco.nom_gco AS genre,
+	cof.avecderivation_cof as avec_derivation,
+	cof.typecsgeneral_cof as type_cs_general,
+	cof.nombrefusible_cof as nombre_fusibles,
+	cof.intensitetaxe_cof as intensite_taxe,
+	cof.intensiteinstalle_cof as intensite_installee,
+	cof.intensitemax_cof as intensite_max,
+	cof.intensiteproduction_cof as intensite_production,
+	cof.nopolice_cof as no_police,
+	cof.pmaxfeg_cof as puissance_max_feg,
+	cof.resisolation_cof as resistance_isolation,
+	--Annexes
+	anx_agg.annexe_chemins as annexes,
+	--Geometry
+	npfv.the_geom as geom_point_geoschematique
+	
+FROM dbo.v_objetreseauversionliaison v_obrvl
+	LEFT JOIN dbo.objetreseauversion_obrv obrv ON v_obrvl.id_obrv = obrv.id_obrv
+	LEFT JOIN (
+		SELECT *
+		FROM dbo.noeudfeatureversion_ndfv ndfv1
+		WHERE ndfv1.idprj_ndfv = 1
+		AND ndfv1.idsch_ndfv = 1) ndfv ON  ndfv.idobr_ndfv = v_obrvl.idparent_cmp
+	LEFT JOIN (
+		SELECT * 
+		FROM dbo.noeudconnectionpointfeatureversion_npfv npfv1
+		WHERE npfv1.idprj_npfv = 1
+		AND npfv1.idsch_npfv != 1
+	) npfv ON npfv.idobr_npfv = obrv.idobr_obrv
+	LEFT JOIN dbo.noeudversion_nodv nodv ON nodv.id_obrv = v_obrvl.id_obrv
+	LEFT JOIN dbo.netat_eta eta ON  eta.id_eta = obrv.idetat_obrv
+	LEFT JOIN dbo.netatentretien_ete ete ON  ete.id_ete = obrv.idetatentretien_obrv
+	LEFT JOIN dbo.nproprietetype_prt prt ON  prt.id_prt = obrv.idproprietetype_obrv
+	LEFT JOIN dbo.npersonneabstraite_pra prap ON obrv.idproprietairepra_obrv = prap.id_pra
+	LEFT JOIN dbo.npersonneabstraite_pra prae ON obrv.idexploitantpra_obrv = prae.id_pra
+	LEFT JOIN dbo.npersonneabstraite_pra praf ON obrv.idfournisseurpra_obrv = praf.id_pra
+	LEFT JOIN dbo.projet_prj prj ON prj.id_prj = obrv.idprj_obrv
+	LEFT JOIN dbo.coffretintroduction_cof cof ON v_obrvl.id_obrv = cof.id_obrv
+	LEFT JOIN dbo.ngenrecoffret_gco gco ON cof.idgco_cof = gco.id_gco
+	LEFT JOIN export.vw_annexes_agg anx_agg ON anx_agg.guid_objet = lower(obrv.racineguid_obrv)
+
+WHERE obrv.idorc_obrv IN (14) 
+	AND obrv.idprj_obrv = 1; -- coffret
+
---
+++ postgresql/views/vw_composants_geoschematique.sql
@@ -0,0 +1,63 @@
+DROP VIEW IF EXISTS export.vw_composants_geoschematique;
+
+CREATE OR REPLACE VIEW export.vw_composants_geoschematique AS
+
+SELECT
+	obrv.id_obrv as id_obrv,
+	obrv.idobr_obrv as id_obr,
+	orc.libelle_orc as type_orc,
+	obrv.modele_obrv AS modele,
+	obrv.code_obrv AS code,
+	obrv.nom_obrv as nom,
+	obrv.infobulle_obrv as infobulle,
+	lower(obrv.racineguid_obrv) as guid_racine,
+	eta.libelle_eta as etat_deploiement,
+	ete.libelle_ete as etat_entretien,
+	prt.libelle_prt	as type_propriete,
+	obrv.convention_obrv as num_esti,
+	obrv.constructiondate_obrv as date_construction,
+	obrv.miseenservicedate_obrv as date_mise_en_service,
+	obrv.horsservicedate_obrv as date_mise_hors_service,
+	obrv.observation_obrv as observation,
+	npfv.niveautension_npfv as tension,
+	prap.libelle_pra as proprietaire,
+	prae.libelle_pra as exploitant,
+	praf.libelle_pra as fournisseur,
+	obrv.creationdate_obrv AS date_creation,
+	obrv.modificationdate_obrv AS date_modification,
+	--Composants
+	copv.etatflux_copv AS etat_flux,
+	--Attributs spécifiques
+	--Annexes
+	--anx_agg.annexe_chemins as annexes,
+	--Geometry
+	npfv.the_geom as geom_point_geoschematique
+	
+FROM dbo.v_objetreseauversionliaison v_obrvl
+	LEFT JOIN dbo.objetreseauversion_obrv obrv ON v_obrvl.id_obrv = obrv.id_obrv
+	LEFT JOIN dbo.nobjetreseauclasse_orc orc ON orc.id_orc = obrv.idorc_obrv
+	-- lien vers le noeud de l'objet parent (armoire)
+	--LEFT JOIN (
+	--	SELECT *
+	--	FROM dbo.noeudfeatureversion_ndfv ndfv1
+	--	WHERE ndfv1.idprj_ndfv = 1
+	--	AND ndfv1.idsch_ndfv = 1) ndfv ON  ndfv.idobr_ndfv = v_obrvl.idparent_cmp
+	LEFT JOIN (
+		SELECT * 
+		FROM dbo.noeudconnectionpointfeatureversion_npfv npfv1
+		WHERE npfv1.idprj_npfv = 1
+		AND npfv1.idsch_npfv != 1
+	) npfv ON npfv.idobr_npfv = obrv.idobr_obrv
+	LEFT JOIN dbo.noeudversion_nodv nodv ON nodv.id_obrv = v_obrvl.id_obrv
+	LEFT JOIN dbo.netat_eta eta ON  eta.id_eta = obrv.idetat_obrv
+	LEFT JOIN dbo.netatentretien_ete ete ON  ete.id_ete = obrv.idetatentretien_obrv
+	LEFT JOIN dbo.nproprietetype_prt prt ON  prt.id_prt = obrv.idproprietetype_obrv
+	LEFT JOIN dbo.npersonneabstraite_pra prap ON obrv.idproprietairepra_obrv = prap.id_pra
+	LEFT JOIN dbo.npersonneabstraite_pra prae ON obrv.idexploitantpra_obrv = prae.id_pra
+	LEFT JOIN dbo.npersonneabstraite_pra praf ON obrv.idfournisseurpra_obrv = praf.id_pra
+	LEFT JOIN dbo.projet_prj prj ON prj.id_prj = obrv.idprj_obrv
+	LEFT JOIN dbo.composantversion_copv copv ON copv.id_obrv = obrv.idobr_obrv
+	LEFT JOIN export.vw_annexes_agg anx_agg ON anx_agg.guid_objet = lower(obrv.racineguid_obrv)
+
+WHERE obrv.idorc_obrv IN (29,30,38) 
+	AND obrv.idprj_obrv = 1; -- discjoncteur, fusible et sectionneur
