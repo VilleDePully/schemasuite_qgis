959d9a78eb4f0fa6a92bc989e2c6da9a9116e474
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2025-04-14 08:11:00 +0200

    Change symbols organisation and colorize with SchemaSuite

---
+++ postgresql/views_HKD/vw_stations.sql
@@ -0,0 +1,58 @@
+DROP VIEW IF EXISTS export.vw_stations;
+
+CREATE OR REPLACE VIEW export.vw_stations AS
+
+SELECT
+	obrv.id_obrv as id_obrv,
+	obrv.idobr_obrv as id_obr,
+	obrv.modele_obrv AS modele,
+	obrv.code_obrv AS code,
+	obrv.nom_obrv as nom,
+	lower(obrv.racineguid_obrv) as guid_racine,
+	eta.libelle_eta as etat_deploiement,
+	ete.libelle_ete as etat_entretien,
+	prt.libelle_prt	as type_propriete,
+	obrv.convention_obrv as num_esti,
+	obrv.constructiondate_obrv as date_construction,
+	obrv.miseenservicedate_obrv as date_mise_en_service,
+	obrv.observation_obrv as observation,
+	ndfv.niveautension_ndf as tension,
+	prap.libelle_pra as proprietaire,
+	prae.libelle_pra as exploitant,
+	praf.libelle_pra as fournisseur,
+	nodv.geoposz_nodv as altitude,
+	nodv.acces_nodv as acces,
+	nodv.emplacement_nodv as emplacement,
+	nodv.typeconstruction_nodv as type_construction,
+	nodv.remarquecontrole_nodv as remarque_controle,
+	nodv.datedecontrole_nodv as date_controle,
+	obrv.creationdate_obrv AS date_creation,
+	obrv.modificationdate_obrv AS date_modification,
+	--Annexes
+	anx.annexe_chemins as annexes,
+	--Transformateurs
+	tragg.transfo_infos as transformateurs,
+	--Cellules
+	cagg.cellule_infos as cellules,
+	--Geometry
+	st_centroid(ST_Force2D(ndfv.the_geom))::geometry('Point',2056) as geom_centroid,
+	ST_Force2D(ndfv.the_geom)::geometry('Polygon',2056) as geom_polygon
+	
+FROM dbo.objetreseauversion_obrv obrv
+	LEFT JOIN dbo.noeudversion_nodv nodv ON nodv.id_obrv = obrv.id_obrv
+	LEFT JOIN dbo.noeudfeatureversion_ndfv ndfv ON  ndfv.idobr_ndfv = obrv.idobr_obrv
+	LEFT JOIN dbo.netat_eta eta ON  eta.id_eta = obrv.idetat_obrv
+	LEFT JOIN dbo.netatentretien_ete ete ON  ete.id_ete = obrv.idetatentretien_obrv
+	LEFT JOIN dbo.nproprietetype_prt prt ON  prt.id_prt = obrv.idproprietetype_obrv
+	LEFT JOIN dbo.npersonneabstraite_pra prap ON obrv.idproprietairepra_obrv = prap.id_pra
+	LEFT JOIN dbo.npersonneabstraite_pra prae ON obrv.idexploitantpra_obrv = prae.id_pra
+	LEFT JOIN dbo.npersonneabstraite_pra praf ON obrv.idfournisseurpra_obrv = praf.id_pra
+	LEFT JOIN dbo.projet_prj prj ON prj.id_prj = obrv.idprj_obrv
+	LEFT JOIN export.vw_annexes_chemins anx ON anx.guid_objet = lower(obrv.racineguid_obrv)
+	LEFT JOIN export.vw_transformateurs_agg tragg ON tragg.id_parent = obrv.id_obrv
+	LEFT JOIN export.vw_cellules_agg cagg ON cagg.id_parent = obrv.id_obrv
+WHERE obrv.idorc_obrv = 9 
+	AND obrv.idprj_obrv = 1 
+	AND ndfv.idprj_ndfv = 1; -- stations
+
+
